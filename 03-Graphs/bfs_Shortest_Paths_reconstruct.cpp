4d9333a0931331d1f4f9c42f75548da98479f5c9
Marlpzg <milopezg@uca.edu.sv>
2025-05-25 15:20:44 -0600

    ADD(BFS Shortest Path reconstruction)

---
+++ 03-Graphs/bfs_Shortest_Paths_reconstruct.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> bfs(const vector<vector<int>> &AL, int start)
+{
+    vector<int> sources(AL.size(), -1);
+    queue<int> q;
+    q.push(start);
+    sources[start] = 0;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        for (int neighbor : AL[node])
+        {
+            if (sources[neighbor] == -1)
+            {
+                sources[neighbor] = node;
+                q.push(neighbor);
+            }
+        }
+    }
+    return sources;
+}
+
+int main()
+{
+    int nNodes;
+    cout << "Cantidad de nodos: ";
+    cin >> nNodes;
+    cout << "\n";
+
+    vector<vector<int>> AL(nNodes, vector<int>());
+
+    int a, b;
+    cout << "Ingresar arista (a b): ";
+    cin >> a >> b;
+
+    while (a != -1 && b != -1)
+    {
+        AL[a].push_back(b);
+        cout << "Ingresar arista (a b): ";
+        cin >> a >> b;
+    }
+
+    cout << "\n";
+
+    int start;
+    cout << "Ingresar nodo de inicio: ";
+    cin >> start;
+    int end;
+    cout << "Ingresar nodo de fin: ";
+    cin >> end;
+
+    cout << "\n";
+
+    vector<int> sources = bfs(AL, start);
+    cout << "Ruta más corta del inicio al fin:\n";
+
+    if (sources[end] == -1)
+    {
+        cout << "No existe un camino del nodo " << start << " al nodo " << end << "\n";
+    }
+    else
+    {
+        stack<int> path;
+        while (end != start)
+        {
+            path.push(end);
+            end = sources[end];
+        }
+
+        cout << start;
+        while (!path.empty())
+        {
+            cout << " -> " << path.top();
+            path.pop();
+        }
+    }
+
+    return 0;
+}
